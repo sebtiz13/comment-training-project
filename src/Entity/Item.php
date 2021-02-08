d90e91616f884c7c0a605f9dd29811124084ef89
Sébastien Rodriguez <sebtiz13@gmail.com>
2021-02-09 00:10:44 +0100

    feat(items): add image property

--- src/Entity/Item.php
+++ src/Entity/Item.php
@@ -34,6 +34,11 @@ class Item
      */
     private ?string $description;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $image;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -71,6 +76,18 @@ class Item
     public function setDescription(?string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
