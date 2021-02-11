b1c337334eabf4fa7f29a4cd4910bbd92b26757a
Sébastien Rodriguez <sebtiz13@gmail.com>
2021-02-11 01:31:30 +0100

    feat(items): add page show item

--- src/Controller/ItemsController.php
+++ src/Controller/ItemsController.php
@@ -4,18 +4,37 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\ItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ItemsController extends AbstractController
 {
+    public function __construct(
+        private ItemRepository $repository,
+    ) {
+    }
+
     /**
      * @Route("/items", name="items.index")
      */
     public function index(): Response
     {
         return $this->render('items/index.html.twig', [
+            'current_menu' => 'items',
+        ]);
+    }
+
+    /**
+     * @Route("/item/{slug}", name="item.show", requirements={"slug": "[a-z0-9\-]*"})
+     */
+    public function show(string $slug): Response
+    {
+        $item = $this->repository->findOneBy(['slug' => $slug]);
+
+        return $this->render('items/show.html.twig', [
+            'item' => $item,
             'current_menu' => 'items',
         ]);
     }
