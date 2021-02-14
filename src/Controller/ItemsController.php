aae3de545a206880c47f7b925f32171bd7688d93
Sébastien Rodriguez <sebtiz13@gmail.com>
2021-02-14 19:27:45 +0100

    feat(comment): send email comment

--- src/Controller/ItemsController.php
+++ src/Controller/ItemsController.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\CommentType;
+use App\Notification\CommentNotification;
 use App\Repository\ItemRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ItemsController extends AbstractController
@@ -38,13 +42,30 @@ class ItemsController extends AbstractController
     /**
      * @Route("/item/{slug}", name="item.show", requirements={"slug": "[a-z0-9\-]*"})
      */
-    public function show(string $slug): Response
+    public function show(string $slug, Request $request, CommentNotification $notification): Response
     {
         $item = $this->repository->findOneBy(['slug' => $slug]);
 
+        $comment = new Comment();
+        $comment->setItem($item);
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $notification->notify($comment);
+                $this->addFlash('email.success', 'Your comment has been sent');
+
+                // return $this->redirectToRoute('item.show', ['slug' => $slug]);
+            } catch (TransportExceptionInterface) {
+                $this->addFlash('email.error', 'Error during send email');
+            }
+        }
+
         return $this->render('items/show.html.twig', [
-            'item' => $item,
             'current_menu' => 'items',
+            'item' => $item,
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Entity/Comment.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Comment
+{
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private ?string $email;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(min=5)
+     */
+    private ?string $message;
+
+    private ?Item $item;
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getItem(): ?Item
+    {
+        return $this->item;
+    }
+
+    public function setItem(?Item $item): self
+    {
+        $this->item = $item;
+
+        return $this;
+    }
+}
